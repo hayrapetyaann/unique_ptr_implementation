da8da7aa2f9bec3693f20628874857304c41e98d
Elen Hayrapetyan <160489923+hayrapetyaann@users.noreply.github.com>
2024-06-07 11:19:42 +0400

    function implementation

---
+++ make_unique/make_impl.cpp
@@ -0,0 +1,6 @@
+#include "make.hpp"
+
+template <typename T, typename... Args>
+std::unique_ptr<T> my_make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
